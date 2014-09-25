dbd7bbc81674eb7114beae9b78159b40a20600d8
Qiaolin Pan <panqiaolin198909@gmail.com>
2014-09-25 23:30:32 +0800

    listener调整

--- src/com/ylp/date/server/ServerConfigRation.java
+++ src/com/ylp/date/server/ServerConfigRation.java
@@ -1,5 +1,7 @@
 package com.ylp.date.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +14,13 @@ import org.springframework.stereotype.Component;
 @Component(SpringNames.ServerConfigRation)
 @Lazy(false)
 public class ServerConfigRation {
+	private static final Logger logger=LoggerFactory.getLogger(ServerConfigRation.class);
 
 	public int getLineLength() {
 		return 5;
+	}
+	public void init(){
+		logger.info("ServerConfigRation init");
 	}
 
 }
