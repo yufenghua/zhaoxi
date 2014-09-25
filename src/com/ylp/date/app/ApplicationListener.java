8277e53b01cfe9a595f7875cec72f4d3e7db0e0d
Qiaolin Pan <panqiaolin198909@gmail.com>
2014-09-25 23:30:32 +0800

    listener调整

--- src/com/ylp/date/app/ApplicationListener.java
+++ src/com/ylp/date/app/ApplicationListener.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -21,12 +22,14 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  * @author Qiaolin Pan
  * 
  */
-public class ApplicationListener implements ServletContextListener {
+public class ApplicationListener extends ContextLoaderListener implements
+		ServletContextListener {
 	private static final Logger logger = LoggerFactory
 			.getLogger(ApplicationListener.class);
 	private static ApplicationContext context;
 
 	public void contextDestroyed(ServletContextEvent arg0) {
+		super.contextDestroyed(arg0);
 		// sina appengine 上面获取的xml解析工厂竟然为空 增加一次判定
 		context = null;
 		logger.debug("get the Spring context" + context);
@@ -34,6 +37,7 @@ public class ApplicationListener implements ServletContextListener {
 	}
 
 	public void contextInitialized(ServletContextEvent arg0) {
+		super.contextInitialized(arg0);
 		context = WebApplicationContextUtils.getWebApplicationContext(arg0
 				.getServletContext());
 		logger.debug("get the Spring context" + context);
