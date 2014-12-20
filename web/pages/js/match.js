5e26bd7fed55fcfc75b1b74a07926518efe10f5f
Qiaolin Pan <panqiaolin198909@gmail.com>
2014-12-20 14:53:01 +0800

    显示姓名，年龄和tag提示信息

--- web/pages/js/match.js
+++ web/pages/js/match.js
@@ -11,72 +11,9 @@
  </div>
  <a class="send-flower" href="" title="送花"></a>
  </li>
- <li>
- <div class="photo">
- <img src="../static/img/photo/photo.png">
- </div>
- <div class="info">
- <p>喜欢做xxx</p>
- <p>偶像xxx</p>
- <p>其他xxx</p>
- </div>
- <a class="send-flower" href="" title="送花"></a>
- </li>
- <li>
- <div class="photo">
- <img src="../static/img/photo/photo.png">
- </div>
- <div class="info">
- <p>喜欢做xxx</p>
- <p>偶像xxx</p>
- <p>其他xxx</p>
- </div>
- <a class="send-flower" href="" title="送花"></a>
- </li>
- <li>
- <div class="photo">
- <img src="../static/img/photo/photo.png">
- </div>
- <div class="info">
- <p>喜欢做xxx</p>
- <p>偶像xxx</p>
- <p>其他xxx</p>
- </div>
- <a class="send-flower" href="" title="送花"></a>
- </li>
  </ul>
  <!--下列表-->
  <ul class="list clearfix">
- <li>
- <div class="photo">
- <img src="../static/img/photo/photo2.png">
- </div>
- <div class="info">
- <p>喜欢做xxx</p>
- <p>偶像xxx</p>
- <p>其他xxx</p>
- </div>
- </li>
- <li>
- <div class="photo">
- <img src="../static/img/photo/photo2.png">
- </div>
- <div class="info">
- <p>喜欢做xxx</p>
- <p>偶像xxx</p>
- <p>其他xxx</p>
- </div>
- </li>
- <li>
- <div class="photo">
- <img src="../static/img/photo/photo2.png">
- </div>
- <div class="info">
- <p>喜欢做xxx</p>
- <p>偶像xxx</p>
- <p>其他xxx</p>
- </div>
- </li>
  <li>
  <div class="photo">
  <img src="../static/img/photo/photo2.png">
@@ -281,6 +218,7 @@ function MatchUser(userObj, mgr, isSame) {
 	this.mgr = mgr;
 	this.isSame = isSame;
 }
+MatchUser.LINE_SIDE=40;
 /**
  * <li> <div class="photo"> <img src="../static/img/photo/photo.png"> </div>
  * <div class="info">
@@ -311,8 +249,8 @@ MatchUser.prototype.init = function() {
 			};
 			$.each(opsiteSex,function(userObjId,value){
 				var tagetPosition=value.getPosition();
-				//是否重合的算法 逻辑是 二者的定位误差不超过20像素
-				if(Math.abs(position.left-tagetPosition.left)<=20&&Math.abs(position.top-tagetPosition.top)<=20){
+				//是否重合的算法 逻辑是 二者的定位误差不超过40像素
+				if(Math.abs(position.left-tagetPosition.left)<=MatchUser.LINE_SIDE&&Math.abs(position.top-tagetPosition.top)<=MatchUser.LINE_SIDE){
 					if(confirm('确认连线'+self.userObj.name+'和'+value.userObj.name+'吗？')){
 						self.mgr.match(self.userObj.id,userObjId);
 					}
@@ -332,11 +270,41 @@ MatchUser.prototype.init = function() {
 	this.liDom.append(this.imgDiv);
 
 	this.tagsDiv = $('<div>');
+	//用户名
+	var userCapTag = $('<p>');
+	var userCaption=this.userObj.name;
+	var userAgeRange='未知年龄段';
+	switch(this.userObj.age){
+		case(1):{
+			userAgeRange='18-20';
+			break;
+		}
+		case(2):{
+			userAgeRange='23-26';
+			break;
+		}
+		case(3):{
+			userAgeRange='20-23';
+			break;
+		}
+		case(4):{
+			userAgeRange='26-30';
+			break;
+		}
+		case(5):{
+			userAgeRange='30';
+			break;
+		}
+	}
+
+	userCapTag.text(userCaption+' '+userAgeRange);
+	this.tagsDiv.append(userCapTag);
+
 	var tags = this.userObj.tags;
 	var tag;
 	for (var i = 0; i < tags.length; i++) {
 		tag = $('<p>');
-		tag.text(tags[i]);
+		tag.html(tags[i].tagsug+":&nbsp&nbsp&nbsp&nbsp"+tags[i].tagInfo);
 		this.tagsDiv.append(tag);
 	}
 	this.liDom.append(this.tagsDiv);
